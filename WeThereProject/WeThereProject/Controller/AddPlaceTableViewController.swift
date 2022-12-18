f8f65c4c6d4195bf6a625a22f2d99136df36f29c
Judy <joo00ee@naver.com>
2022-12-18 09:32:57 +0900

    refactor: UIImage extension을 파일로 분리

--- WeThereProject/WeThereProject/Controller/AddPlaceTableViewController.swift
+++ WeThereProject/WeThereProject/Controller/AddPlaceTableViewController.swift
@@ -417,18 +417,6 @@ extension AddPlaceTableViewController: GMSAutocompleteViewControllerDelegate { /
     } //캔슬버튼 눌렀을 때 화면 꺼지게
 }
 
-fileprivate extension UIImage {
-    func resize(newWidth: CGFloat) -> UIImage {
-        let scale = newWidth / self.size.width
-        let newHeight = self.size.height * scale
-        let size = CGSize(width: newWidth, height: newHeight)
-        let render = UIGraphicsImageRenderer(size: size)
-        let renderImage = render.image {
-            context in self.draw(in: CGRect(origin: .zero, size: size)) }
-        return renderImage
-    }
-}
-
 extension AddPlaceTableViewController :  UIPickerViewDelegate, UIPickerViewDataSource{
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
