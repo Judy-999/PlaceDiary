43b40a3759d98c76a7bfabc73e92f1bcfddae773
Judy <joo00ee@naver.com>
2023-03-23 21:38:23 +0900

    refactor: PlaceRepository 구현

--- WeThereProject/WeThereProject/Data/Place.swift
+++ WeThereProject/WeThereProject/Data/Place.swift
@@ -19,27 +19,3 @@ struct Place {
     var geopoint: GeoPoint
     var group: String
 }
-
-extension Place {
-    init?(from document: QueryDocumentSnapshot) {
-        guard let name = document["name"] as? String,
-              let location = document["position"] as? String,
-              let date = document["date"] as? Timestamp,
-              let isFavorit = document["favorit"] as? Bool,
-              let category = document["category"] as? String,
-              let rate = document["rate"] as? String,
-              let coment = document["coment"] as? String,
-              let geopoint = document["geopoint"] as? GeoPoint,
-              let group = document["group"] as? String else { return nil }
-        
-        self.name = name
-        self.location = location
-        self.date = date.dateValue()
-        self.isFavorit = isFavorit
-        self.category = category
-        self.rating = rate
-        self.coment = coment
-        self.geopoint = geopoint
-        self.group = group
-    }
-}
---
+++ WeThereProject/WeThereProject/Data/PlaceRepository.swift
@@ -0,0 +1,105 @@
+//
+//  PlaceRepository.swift
+//  WeThereProject
+//
+//  Created by 김주영 on 2023/03/23.
+//
+
+import RxSwift
+import FirebaseFirestore
+
+struct PlaceRepository {
+    func loadPlaces() -> Observable<[Place]> {
+        let snapshot = FirestoreManager.shared.loadCollection()
+        return snapshot.map { document in
+            document.compactMap { toPlaces($0) }
+        }
+    }
+    
+    func loadClassification() -> Observable<([String], [String])> {
+        let document = FirestoreManager.shared.loadDocument(at: Classification.collection)
+        
+        return document
+            .do(onError: { error in
+                if error as? ClassificationError == .empty {
+                    setupBasicClassification()
+                }
+            })
+            .compactMap { toClassfication($0) }
+            .catchAndReturn((Classification.basicCategory, Classification.basicGroup))
+    }
+    
+    func updateClassification(_ classification: String,
+                              with items: [String]) -> Observable<Void> {
+        let data = [classification: items]
+        return FirestoreManager.shared.update(data,
+                                              at: Classification.collection,
+                                              document: nil)
+    }
+    
+    func updateFavorit(_ favorit: Bool, placeName: String) -> Observable<Void> {
+        let data = [PlaceData.favorit: favorit]
+        return FirestoreManager.shared.update(data,
+                                              at: nil,
+                                              document: placeName)
+    }
+    
+    func deletePlace(_ placeName: String) -> Observable<Void> {
+        return FirestoreManager.shared.delete(placeName)
+    }
+    
+    func savePlace(_ place: Place) -> Observable<Void> {
+        let saveData: [String: Any] = [
+            PlaceData.name: place.name,
+            PlaceData.location: place.location,
+            PlaceData.date: place.date,
+            PlaceData.favorit: place.isFavorit,
+            PlaceData.rating: place.rating,
+            PlaceData.coment: place.coment,
+            PlaceData.category: place.category,
+            PlaceData.geopoint: place.geopoint,
+            PlaceData.group: place.group
+        ]
+        
+        return FirestoreManager.shared.save(saveData,
+                                            at: nil,
+                                            document: place.name)
+    }
+    
+    private func setupBasicClassification() {
+        _ = FirestoreManager.shared.save(Classification.basic,
+                                                at: Classification.collection,
+                                                document: nil)
+    }
+}
+
+extension PlaceRepository {
+    private func toClassfication(_ document: DocumentSnapshot) -> ([String], [String])? {
+        guard let categories = document.get(PlaceData.category) as? [String],
+              let groups = document.get(PlaceData.group) as? [String] else { return nil }
+        
+        return (categories, groups)
+    }
+    
+    private func toPlaces(_ document: DocumentSnapshot) -> Place? {
+        guard let name = document[PlaceData.name] as? String,
+              let location = document[PlaceData.location] as? String,
+              let date = document[PlaceData.date] as? Timestamp,
+              let isFavorit = document[PlaceData.favorit] as? Bool,
+              let category = document[PlaceData.category] as? String,
+              let rating = document[PlaceData.rating] as? String,
+              let coment = document[PlaceData.coment] as? String,
+              let geopoint = document[PlaceData.geopoint] as? GeoPoint,
+              let group = document[PlaceData.geopoint] as? String else { return nil }
+        
+       return Place(name: name,
+                    location: location,
+                    date: date.dateValue(),
+                    isFavorit: isFavorit,
+                    category: category,
+                    rating: rating,
+                    coment: coment,
+                    geopoint: geopoint,
+                    group: group)
+    }
+}
